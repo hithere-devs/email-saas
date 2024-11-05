6b3dcdec83c160a349aaa7ce1187f39ae4c45446
Azhar Mahmood <mywritingfrenzy@gmail.com>
2024-11-05 23:06:58 +0530

    Fixing pages, Loading States

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/", // Landing page
   "/api/clerk/webhook(.*)",
   "/api/initial-sync(.*)",
 ]);
